ed6c7618964b1d3df39b3394fa45a3d26b4cd4aa
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2021-02-01 18:51:29 -0600

    Added Utilities package files: BrowserUtils, ConfigurationReader, WebDriverFactory and WebOrderUtils.

---
+++ src/test/java/com/trycloud/tests/UserStory6/UserStory6.java
@@ -0,0 +1,50 @@
+package com.trycloud.tests.UserStory6;
+
+import com.trycloud.tests.base.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+
+public class UserStory6 extends TestBase {
+
+    //User Story 6: As a user, I should be able to access to Notes module.
+
+    //6.1 :Set up chrome browser
+    //6.2 : Get https://trycloud.net/
+    //6.3: Login as a user
+
+
+
+    @Test
+    public void accessTalksModule () {
+        // Successful Login
+
+        //Test case #1 - verify users can access to Talks module
+        //2. Click “Notes” module
+
+        WebElement notesIcon = driver.findElement(By.xpath(("//a[href='http://qa.trycloud.net/index.php/apps/notes/'])")));
+        //notesIcon.click();
+
+
+
+
+        //String expectedInTitle = "Notes";
+        //String actualInTitle = "";
+
+        //if(actu)
+        //3. Verify the page tile/URL is Notes module’s tile
+    }
+
+    @Test
+    public void sendNotes () {
+        //Test case #2 - verify users can send notes in the page
+
+        //2. Click notes module
+        //3. Click “New Note” button
+        //4. Write a message/Note
+        //5. Verify the note is added on the note list
+
+    }
+
+}
